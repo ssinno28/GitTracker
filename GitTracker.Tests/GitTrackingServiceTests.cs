99dcbb6d8e2ec0c1255bab7d669531a1f6c7d7b5
ssinno28 <sammi.sinno@gmail.com>
2020-10-04 18:01:27 -0400

    adding an extra check to make sure we don't process files taht don't have a value provider

--- GitTracker.Tests/GitTrackingServiceTests.cs
+++ GitTracker.Tests/GitTrackingServiceTests.cs
@@ -212,6 +212,45 @@ namespace GitTracker.Tests
             Assert.Equal(1, conflicts.Count);
             Assert.Equal(2, conflicts.First().ChangedProperties.Count);
             Assert.Equal(1, conflicts.First().ValueProviderConflicts.Count);
+        }        
+        
+        [Fact]
+        public async Task Test_Sync_On_Repo_Merge_Conflict_Take_Normal_No_Value_Provider()
+        {
+            GitConfig.LocalPath = SecondLocalPath;
+
+            await GitTrackingService.Sync(Email);
+
+            GitConfig.LocalPath = LocalPath;
+
+            string contentItemPath = PathProvider.GetTrackedItemPath(typeof(BlogPost), _initialTrackedItem);
+            string filePath = Path.Combine(contentItemPath, "body.mds");
+
+            await File.WriteAllTextAsync(filePath, "My Test Body");
+            _initialTrackedItem = await GitTrackingService.Update(_initialTrackedItem);
+
+            GitTrackingService.Stage(_initialTrackedItem);
+            GitRepo.Commit("Repo 1 Second Commit", Email);
+            await GitTrackingService.Publish(Email);
+
+            GitConfig.LocalPath = SecondLocalPath;
+
+            contentItemPath = PathProvider.GetTrackedItemPath(typeof(BlogPost), _initialTrackedItem);
+            filePath = Path.Combine(contentItemPath, "body.md");
+
+            await File.WriteAllTextAsync(filePath, "My Test Body 2");
+            _initialTrackedItem = await GitTrackingService.Update(_initialTrackedItem);
+
+            GitTrackingService.Stage(_initialTrackedItem);
+            GitRepo.Commit("Repo 2 Second Commit", Email);
+
+            bool result = await GitTrackingService.Sync(Email);
+            Assert.False(result);
+
+            var conflicts = await GitTrackingService.GetTrackedItemConflicts();
+            Assert.Equal(1, conflicts.Count);
+            Assert.Equal(1, conflicts.First().ChangedProperties.Count);
+            Assert.Equal(0, conflicts.First().ValueProviderConflicts.Count);
         }
 
         [Fact]
